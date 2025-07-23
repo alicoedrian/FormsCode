ca25aa12044ed879116755d241d1c7e7a63e659f
alihernan <hpabon@alico-sa.com>
2025-07-23 10:51:57 -0500

    proyecto mas robusto

--- run.py
+++ run.py
@@ -2,9 +2,9 @@
 
 from mi_aplicacion import create_app
 
-# Creamos una instancia de la aplicación usando la factory
+
 app = create_app()
 
 if __name__ == '__main__':
-    # Usar host='0.0.0.0' para que sea accesible en tu red local.
+   
     app.run(debug=True, host='0.0.0.0', port=5001)
